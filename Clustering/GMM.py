189f8481163c818bbfb7e1d1fe1e5f597217998b
WinterCyan <wintercyan98@gmail.com>
2022-04-01 15:53:30 +0800

    adjust GMM

--- Clustering/GMM.py
+++ Clustering/GMM.py
@@ -4,13 +4,14 @@ from numpy import genfromtxt
 from matplotlib import pyplot as plt
 
 class GMM:
-    def __init__(self, n_feature, n_class):
+    def __init__(self, n_feature, n_class, eps=1e-6):
         """
         :param n_feature: D
         :param n_class: K
         """
         self.K = n_class
         self.D = n_feature
+        self.eps = eps
 
         self.gaussian_means = None  # K*D
         self.gaussian_vars = None  # K*D*D
@@ -20,17 +21,41 @@ class GMM:
         self.X = None  # shape: N*D
         self.N = None
 
-    def __init_gaussian(self):
-        self.gaussian_means = np.array([[1,0],[0,1],[0,0],[1,1]]).astype(float)
-        single_sigma = np.array([[1,0],[0,1]]).astype(float)
-        self.gaussian_vars = np.zeros((self.K, self.D, self.D)).astype(float)
-        for i in range(self.K):
-            self.gaussian_vars[i,:,:] = single_sigma
-        self.gaussian_weights = np.ones(self.K).astype(float)/self.K
+    def __init_gaussian(self,X):
+        self.X = X
+        self.N = X.shape[0]
+        np.random.shuffle(X)
+        means = []
+        covs = []
+        div_size = int(np.floor(self.N/self.K))
+        X_splits = np.array([X[i:i+div_size] for i in range(0, self.N, div_size)])
+        self.gaussian_means = np.zeros((self.K, self.D))
+        self.gaussian_vars = np.zeros((self.K, self.D, self.D))
+        for k in range(self.K):
+            means.append(np.mean(X_splits[k], axis=0))
+            covs.append(np.cov(X_splits[k].T))
+        self.gaussian_means = np.array(means)
+        self.gaussian_vars = np.array(covs)
+
+        self.gaussian_weights = np.ones(self.K)/self.K
+        self.gamma = np.zeros((self.N, self.K))
         print(self.gaussian_means)
         print(self.gaussian_vars)
         print(self.gaussian_weights)
-        self.gamma = np.zeros((self.N, self.K)).astype(float)
+
+    def clustering(self,X):
+        assert X.shape[1] == self.D
+        N = X.shape[0]
+        res = np.zeros(N).astype(int)
+        ma_dist = np.zeros(self.K).astype(float)
+        conv_invs = [np.linalg.inv(var) for var in self.gaussian_vars]
+        for i in range(N):
+            x = X[i,:]
+            for k in range(self.K):
+                ma_dist[k] = np.dot(np.dot((x-self.gaussian_means[k]).T, conv_invs[k]), x-self.gaussian_means[k])
+            res[i] = np.argmin(ma_dist)
+        return res
+
 
     def fit(self,X,train_iters):
         """
@@ -38,14 +63,16 @@ class GMM:
         :param train_iters: iters to train
         :return:
         """
-        self.X = X
-        self.N = X.shape[0]
-        self.__init_gaussian()
+        prev_loglike = -99999
+        self.__init_gaussian(X)
         for iter in range(train_iters):
             self.E_step()
             self.M_step()
-            loss = self.__logLike(self.X, self.gaussian_weights, self.gaussian_means, self.gaussian_vars)
-            print(f"iter:{iter}, loss:{loss}")
+            loglike = self.__logLike(self.X, self.gaussian_weights, self.gaussian_means, self.gaussian_vars)
+            if abs(prev_loglike-loglike)<self.eps:
+                break
+            prev_loglike = loglike
+            print(f"iter:{iter}, loss:{prev_loglike}")
 
     def E_step(self):
         # gamm ji: sample xj -> gaussian gi, shape N*K
@@ -94,9 +121,27 @@ class GMM:
 
 if __name__ == '__main__':
     X = genfromtxt("../data/TrainingData_GMM.csv", delimiter=',')
-    print(f"data shape: {X.shape}")
-    plt.scatter(X[:,0],X[:,1])
+    # print(f"data shape: {X.shape}")
+    # plt.scatter(X[:,0],X[:,1])
+    plt.show()
+    # N, D = X.shape[0], X.shape[1]
+    # model = GMM(n_feature=D, n_class=4)
+    # model.fit(X, train_iters=100)
+    # print(model.gaussian_means)
+    # print(model.gaussian_vars)
+    # print(model.gaussian_weights)
+    # X = genfromtxt("../data/gmm.csv", delimiter=' ')
+    # plt.scatter(X[:,0], X[:,1])
     # plt.show()
     N, D = X.shape[0], X.shape[1]
+    # print(N,D)
     model = GMM(n_feature=D, n_class=4)
-    model.fit(X, train_iters=200)
+    model.fit(X, train_iters=60)
+    pred = model.clustering(X)
+    print(pred)
+    markers = ['o', '+', 'x', '*']
+    colors = ['r', 'b', 'y', 'g']
+    for d, l in zip(X, pred):
+        plt.scatter(d[0], d[1], color=colors[l], marker=markers[l])
+    plt.show()
+
